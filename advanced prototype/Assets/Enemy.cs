a7a9da15497ed4157b1228698154b302fe00c0f7
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-08-15 22:38:56 -0400

    flimsy ai

---
+++ advanced prototype/Assets/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public Transform target;
+    public Transform mytransform; 
+
+
+    private void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+    }
+  }
