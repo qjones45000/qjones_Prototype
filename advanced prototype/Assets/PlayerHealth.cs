0bfc2f7470055e377fedfeb33943f931f118ed68
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-08-27 23:41:25 -0400

    Update

---
+++ advanced prototype/Assets/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int StartingHealth = 100;
+    public int CurrentHealth;
+    public Slider HealthSlider; 
+
+
+	// Use this for initialization
+	void Start () {
+
+        CurrentHealth = StartingHealth; 
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+  public void TakeDamage(int amount)
+    {
+        CurrentHealth -= amount;
+        HealthSlider.value = CurrentHealth;
+    }
+}
