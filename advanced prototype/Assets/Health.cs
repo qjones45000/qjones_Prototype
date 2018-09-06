4a8acf35c1deddd10eb11ae3aab99c46d257b29f
cactusboi <36212691+cactusboi@users.noreply.github.com>
2018-09-06 17:52:06 -0400

    stupid health shit

--- advanced prototype/Assets/Health.cs
+++ advanced prototype/Assets/Health.cs
@@ -1,37 +1,58 @@
 ﻿using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
 
-    public float Max_Health = 100f;
-    public float Currenthealth = 0f;
-    public GameObject HealthBar;
+	public float currenthealth {get; set;}
+	public float Max_health {get; set;} 
+
+	public Slider healthbar;
 
 
-    void start()
-    {
-        Currenthealth = Max_Health;
-        InvokeRepeating("decreaseHealth", 1f, 1f);
-    }
 
-     void Update()
-    {
-        
+	void start ()
+	{
+		Max_health = 20f;
 
-    }
+		currenthealth = Max_health;
 
-    void decreaseHealth()
-    {
-        Currenthealth -= 2f;
-        float calc_Health = Currenthealth / Max_Health;
-        SetHealhBar(calc_Health);
-    }
+		healthbar.value = calc_health ();
+	}
 
-    public void SetHealhBar(float myHealth)
-    {
-        HealthBar.transform.localScale = new Vector3 (Mathf.Clamp(myHealth, 0f, 1f),
-                                               HealthBar.transform.localScale.y,
-                                               HealthBar.transform.localScale.x);
-    }
+		void update()
+		{
+		if (Input.GetKeyDown (KeyCode.X)) 
+		{
+			dealDamage (6);
+		}
+
+
+
+		}
+	float calc_health()
+	{
+		return currenthealth / Max_health;
+	}
+	
+	
+
+
+	void dealDamage (float damage)
+	{
+		currenthealth -= damage;
+
+		if (currenthealth <= 0) 
+		
+		Die ();
+		
+	}
+
+		void Die ()
+		{
+			currenthealth = 0;
+			Debug.Log("you died");
+		}
+
+
 }
