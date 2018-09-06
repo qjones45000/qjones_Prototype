4bade49fbbc848716f01b5e6224498d65a89bc72
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-09-05 21:40:40 -0400

    cool stuff

---
+++ advanced prototype/Assets/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Health : MonoBehaviour {
+
+    public float Max_Health = 100f;
+    public float Currenthealth = 0f;
+    public GameObject HealthBar;
+
+
+    void start()
+    {
+        Currenthealth = Max_Health;
+        InvokeRepeating("decreaseHealth", 1f, 1f);
+    }
+
+     void Update()
+    {
+        
+
+    }
+
+    void decreaseHealth()
+    {
+        Currenthealth -= 2f;
+        float calc_Health = Currenthealth / Max_Health;
+        SetHealhBar(calc_Health);
+    }
+
+    public void SetHealhBar(float myHealth)
+    {
+        HealthBar.transform.localScale = new Vector3 (Mathf.Clamp(myHealth, 0f, 1f),
+                                               HealthBar.transform.localScale.y,
+                                               HealthBar.transform.localScale.x);
+    }
+}
