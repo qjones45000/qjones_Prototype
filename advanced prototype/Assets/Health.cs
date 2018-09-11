e995fdcd08773bd6b565d295206cc8682cff054b
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-09-10 20:44:14 -0400

    health is done fr

--- advanced prototype/Assets/Health.cs
+++ advanced prototype/Assets/Health.cs
@@ -6,17 +6,23 @@ public class Health : MonoBehaviour {
 
     public float currenthealth = 0;
     public float Max_health = 100f;
+    public bool playerHealth;
 
+    public float Player2Health = 0;
+    public float Player2MaxHP = 100f;
     
 
 
 	public Slider healthbar;
+    public Slider player2health;
 
 
 
 	void Start ()
 	{
 		currenthealth = Max_health;
+
+        Player2Health = Player2MaxHP;
 	}
 
 
@@ -26,42 +32,74 @@ public class Health : MonoBehaviour {
 	{
 		return currenthealth / Max_health;
 	}
+
+    float Calc_Health2()
+    {
+        return Player2Health / Player2MaxHP;
+    }
 	
-	
+
 
 
 	public void dealDamage (float damage)
 	{
+        
+
 		currenthealth -= damage;
 
 		if (currenthealth <= 0) 
 		
 		Die ();
+
+        
 		
 	}
+
+    public void DealDamage2 (float Damage2)
+    {
+        Player2Health -= Damage2;
+
+        if (Player2Health <= 0)
+
+            Die();
+    }
 
 		void Die ()
 		{
 			currenthealth = 0;
+        Player2Health = 0;
 			Debug.Log("you died");
 		}
 
-     void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
-        
-        
+        if (playerHealth == true)
+        {
             if (collision.gameObject.tag == "Hand")
             {
                 dealDamage(6);
                 Debug.Log("you have been hit");
                 calc_health();
-
-
             }
-                
+
             healthbar.value = currenthealth;
         }
-        
+
+        else
+        {
+            if (collision.gameObject.tag == "fist")
+            {
+                DealDamage2(6);
+                Debug.Log("you got fucked");
+                Calc_Health2();
+            }
+
+            player2health.value = Player2Health;
+
+        }
+        }
     }
+        
+    
 
 
