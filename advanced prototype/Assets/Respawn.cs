f14968f2986acd27d3ac5ed7599f90d501611381
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-09-13 11:47:02 -0400

    respawn update

---
+++ advanced prototype/Assets/Respawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    public GameObject Player;
+    public Transform spawnpoint;
+   
+
+   
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        GetComponent<move>();
+       
+
+           Player.transform.position = spawnpoint.transform.position;
+            Debug.Log("collided");
+        
+        
+    }
+
+}
