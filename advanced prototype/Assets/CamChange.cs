c1e1b27328f600ad7b54db7664d0597a5b7dde36
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-09-15 12:26:56 -0400

    change of cam

---
+++ advanced prototype/Assets/CamChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamChange : MonoBehaviour {
+
+    public Camera camleft;
+    public Camera camright;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        camleft.enabled = false;
+        camright.enabled = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+           
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.gameObject.tag == "camswitch")
+        {
+
+            camleft.enabled = true;
+            camright.enabled = false;
+        }
+    }
+}
