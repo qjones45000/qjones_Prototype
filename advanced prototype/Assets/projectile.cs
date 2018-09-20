02f634f91e916c5bfa7eace503a2a3467ac65303
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-09-20 17:33:57 -0400

    stuff

--- advanced prototype/Assets/projectile.cs
+++ advanced prototype/Assets/projectile.cs
@@ -13,20 +13,19 @@ public class projectile : MonoBehaviour
     public Rigidbody enemyPower;
     public Transform EnemyForm;
 
-
+    
 
     // Use this for initialization
     void Start()
     {
-      
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(powers == true)
+
+        if (powers == true)
         {
             if (Input.GetKeyDown("l"))
             {
@@ -35,8 +34,8 @@ public class projectile : MonoBehaviour
                 Pmove = Instantiate(power, powerform.position, powerform.rotation) as Rigidbody;
                 Pmove.AddForce(powerform.forward * 5000);
 
-                StartCoroutine(Stop());
-               
+                
+
             }
             else
             {
@@ -47,29 +46,25 @@ public class projectile : MonoBehaviour
                     Emove = Instantiate(enemyPower, EnemyForm.position, EnemyForm.rotation) as Rigidbody;
                     Emove.AddForce(EnemyForm.forward * 5000);
 
-                    StartCoroutine(Stop());
+                    
 
-                   
+
                 }
 
-              
+
             }
         }
-    
 
-       
 
-        }
-
-    IEnumerator Stop()
-    {
-        
-            yield return new WaitForSeconds(5);
-        
     }
 
-    
+  
+
+
     }
+
+
+
 
 
 
---
+++ advanced prototype/Assets/script/ParticleScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour {
+
+    public ParticleSystem Powerlauncher;
+    public ParticleSystem emissionTho;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+     void OnParticleCollision(GameObject other)
+    {
+        EmitAtLocation(); 
+    }
+
+    void EmitAtLocation()
+    {
+        emissionTho.Emit(1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetButton("Fire1"))
+        {
+            Powerlauncher.Emit(1);
+        }
+
+
+		
+	}
+}
